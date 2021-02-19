c190d761d229664b0024d630221c3f64eb9be42d
Joan Vicens <jwcervera@hotmail.com>
2021-02-19 16:46:44 +0100

    all satellites added

---
+++ Assets/Satellite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : MonoBehaviour
+{
+
+    private GameObject planet;
+
+    public string name;
+    //public float distanceToPlanet; //in AU
+
+    private Vector3 position;
+
+    private Vector3 desiredPosition;
+    private Transform center;
+     public float orbitRadius = 10.0f;
+     public float orbitDegreesPerSec = 180.0f;
+
+    void Start()
+    {
+        planet = transform.parent.gameObject;
+        //this.position = new Vector3(distanceToPlanet, 0, 0);
+    }
+
+    void Update() {}
+
+    void LateUpdate()
+    {
+        transform.position = planet.transform.position + (transform.position - planet.transform.position).normalized * orbitRadius;
+        transform.RotateAround(planet.transform.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+    }
+}
--- Assets/SolarSystemOptions.cs
+++ Assets/SolarSystemOptions.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class SolarSystemOptions
 {
     public static double dt = 0.0002d;
+    public static double moonT = 0.002;
 }
